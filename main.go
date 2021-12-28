cc1777e5ed719865afeb2e9845ba0e3a147d2e37
= <=>
2021-12-28 11:01:47 +0100

    working version of queue with TTL

--- main.go
+++ main.go
@@ -1,85 +1,103 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
 type q struct {
 	Timestamp int64  `json:"epoch"`
 	Queue     string `json:"queue"`
 	TTL       int64  `json:"TTL"`
-	ID        uint   `json:"incident_id"`
+	ID        int    `json:"incident_id"`
 }
 
-// albums slice to seed record album data.
-var queue = []q{}
+type TTLMap struct {
+	m map[string]*q
+	l sync.Mutex
+}
+
+func New() (m *TTLMap) {
+	m = &TTLMap{m: make(map[string]*q)}
+	go func() {
+		for range time.Tick(time.Second) {
+			m.l.Lock()
+			for k, v := range m.m {
+				v.TTL--
+				if v.TTL <= 0 {
+					delete(m.m, k)
+				}
+			}
+			m.l.Unlock()
+		}
+	}()
+	return
+}
+
+func (m *TTLMap) Len() int {
+	return len(m.m)
+}
+
+func (m *TTLMap) Put(k string, v q) bool {
+	var s bool
+	m.l.Lock()
+	if _, ok := m.m[k]; !ok {
+		m.m[k] = &v
+		s = true
+	} else {
+		s = false
+	}
+	m.l.Unlock()
+	return s
+}
+
+func (m *TTLMap) Get() []*q {
+	m.l.Lock()
+	var l []*q
+	for _, v := range m.m {
+		l = append(l, v)
+	}
+	m.l.Unlock()
+	return l
+}
+
+var m *TTLMap
 
 func main() {
+	m = New()
 	router := gin.Default()
 	router.GET("/queue", getQueue)
 	//router.GET("/albums/:id", getAlbumByID)
 	router.POST("/queue", postQueue)
-
-	router.Run("localhost:8080")
+	router.Run(":8080")
 }
 
-func isAlive(rec *q) (int64, error) {
-	now := time.Now().Unix()
-	if (rec.Timestamp + rec.TTL) > now {
-		fmt.Println("still valid")
-		return (rec.Timestamp + rec.TTL) - now, errors.New("record still active")
-	} else {
-		return 0, nil
-	}
-}
-func recordEx(rec *q) (bool, int64) {
-	for i, e := range queue {
-		if e.Queue == rec.Queue && e.ID == rec.ID {
-			//send existing one for check if it's still valid
-			t, err := isAlive(&e)
-			if err != nil {
-				fmt.Println(err)
-				return false, t
-			} else {
-				queue[i] = queue[len(queue)-1]
-				queue = queue[:len(queue)-1]
-			}
-		}
-	}
-	return true, 0
-}
-
-// getAlbums responds with the list of all albums as JSON.
 func getQueue(c *gin.Context) {
-	c.JSON(http.StatusOK, queue)
+	c.JSON(http.StatusOK, m.Get())
 }
 
-// postQueue adds an album from JSON received in the request body.
 func postQueue(c *gin.Context) {
 	// fmt.Println(&c)
-	var newQueue q
+	var nq q
 
 	//set default epoch value to Now
 
-	newQueue.Timestamp = time.Now().Unix()
+	//newQueue.Timestamp = time.Now().UnixMilli()
 	// Call BindJSON to bind the received JSON to
 	// newQueue.
-	if err := c.BindJSON(&newQueue); err != nil {
+	if err := c.BindJSON(&nq); err != nil {
 		return
 	}
-	ex, t := recordEx(&newQueue)
-	if ex {
-		// Add the new item to the queue.
-		queue = append(queue, newQueue)
-		c.JSON(http.StatusCreated, newQueue)
+	s := m.Put(fmt.Sprint(nq.Queue+strconv.Itoa(nq.ID)), nq)
+	if s {
+		c.JSON(http.StatusCreated, nq)
 	} else {
-		c.AbortWithStatusJSON(423, gin.H{"message": "record exist", "ttl": t})
+		c.AbortWithStatusJSON(423, gin.H{"message": "record exist"})
 	}
 
 }
